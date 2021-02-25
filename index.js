321ed914be197d2acc4a3b04912b245e45db1932
Alexander <alexanderl96@yahoo.com>
2021-02-25 16:52:45 -0500

    r

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    const input = document.getElementById('number-Dice')
-    const inputNum = Number(input) 
+    const input = document.getElementById('number-Dice') 
     const btn = document.getElementById('roll-dice')
     const p2 = document.querySelector('#dice-rolloed')
     const p = document.querySelector('#sum-para')
@@ -10,34 +9,50 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     btn.addEventListener('click', ()=>{
         if(input.value !== ""){
-            const dice = [{1: '&#9856', 
-                2: '&#9857',
-                3: '&#9858',
-                4: '&#9859',
-                5: '&#9860',
-                6: '&#9861'}]
+            const dice = ['⚀',
+                '⚁',
+                '⚂',
+                '⚃',
+                '⚄',
+                '⚅',]
             arr = []
             for(i = 0; i < input.value; i++){
                 let newArr = dice[Math.floor(Math.random()* dice.length)]
-                arr += Number[newArr]
-    
-                p2.innerHTML = arr[newArr]
-                
-                if( p2.textContent !== 0){
-                    p.textContent += inputNum + `Roll`
+                arr += newArr
+                if(arr.length = 1){
+                    p2.textContent = arr
+                }else{
+                    p2.textContent = arr[newArr]
                 }
+                
 
+                if(p2.textContent === '⚀'){
+                    p.textContent = `Sum = ${1}`
+                    p.textContent.length.value
+                } else if(p2.textContent === '⚁'){
+                    p.textContent = `Sum = ${2}`
+                }else if(p2.textContent === '⚂'){
+                    p.textContent = `Sum = ${3}`
+                }else if(p2.textContent === '⚃'){
+                    p.textContent = `Sum = ${4}`
+                }else if(p2.textContent === '⚄'){
+                    p.textContent = `Sum = ${5}`
+                }else if(p2.textContent === '⚅'){
+                    p.textContent = `Sum = ${6}`
+                
+                }
                 console.log(arr)
                 
             }
             const li = document.createElement('li')
-                li.textContent = arr.value
+                li.textContent = arr
                 ul.appendChild(li)
-                 if(p2.textContent == 1){
-                     li.textContent = []
-                 }else if(p.textContent > 1){
-                     li.textContent = li.textContent + p2.textContent
-                 }
+                if(p.textContent === li.textContent){
+                    return 
+                }else{
+                    li.textContent = `${arr} ${p.textContent}`
+                }
+               
         }
         
 
